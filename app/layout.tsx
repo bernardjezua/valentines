002380ea56d518d236cb906ccceaa4691ff4a8b5
bernardjezua <bernardjezuaml@gmail.com>
2026-02-14 05:06:48 +0800

    chore: changed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,30 @@
-import React from "react"
-import type { Metadata } from 'next'
-import { Open_Sans, Great_Vibes } from 'next/font/google'
+import React from "react";
+import type { Metadata } from "next";
+import { Open_Sans, Great_Vibes } from "next/font/google";
 
-import './globals.css'
+import "./globals.css";
 
-const openSans = Open_Sans({ subsets: ['latin'], variable: '--font-sans' })
-const greatVibes = Great_Vibes({ subsets: ['latin'], weight: '400', variable: '--font-heading' })
+const openSans = Open_Sans({ subsets: ["latin"], variable: "--font-sans" });
+const greatVibes = Great_Vibes({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-heading",
+});
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
-}
+  title: "Our Valentine Memoa.",
+  description: "Valentine Gift (3/4) for Myle",
+  generator: "v0.app",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className={`${openSans.variable} ${greatVibes.variable}`}>
       <body className="font-sans antialiased">{children}</body>
     </html>
-  )
+  );
 }
